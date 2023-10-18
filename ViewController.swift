59813b54948372e60dc708025aba58a1dd60f1fb
Koichi5 <koichi20021217@gmail.com>
2023-10-18 14:17:13 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit_practice
+//
+//  Created by Koichi Kishimoto on 2023/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
